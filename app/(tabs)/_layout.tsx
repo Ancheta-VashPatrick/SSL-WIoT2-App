6c742eacf517f50e89881539e01269d0653a7f2a
Rick Ancheta <ancheta.vashpatrick@gmail.com>
2024-11-23 16:46:57 +0800

    Collect Screen Hidden from Web

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,30 +14,53 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Dashboard",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="dashboard" color={color} />
+          ),
         }}
       />
+      {Platform.OS == "android" || Platform.OS == "ios" ? (
+        <Tabs.Screen
+          name="collect"
+          options={{
+            title: "Collect",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="bluetooth" color={color} />
+            ),
+          }}
+        />
+      ) : (
+        <Tabs.Screen
+          name="collect"
+          options={{
+            tabBarButton: () => null,
+          }}
+        />
+      )}
       <Tabs.Screen
-        name="explore"
+        name="options"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Options",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list" color={color} />
+          ),
         }}
       />
     </Tabs>
