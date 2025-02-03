d3fa512d301fe50a8c9ae0e6cb80e7da8b460f29
Rick Ancheta <ancheta.vashpatrick@gmail.com>
2025-02-03 10:52:54 +0800

    Basic BLE Screen

--- app/(tabs)/collect.tsx
+++ app/(tabs)/collect.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from "react-native";
+import { StyleSheet, Image, Platform, TouchableOpacity, FlatList } from "react-native";
 
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
@@ -6,8 +6,56 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { useState } from "react";
+import DeviceModal from "@/components/DeviceConnectionModal";
+import useBLE from "@/app/useBLE";
 
 export default function CollectScreen() {
+  const {
+    allDevices,
+    connectedDevice,
+    connectToDevice,
+    scanForPeripherals,
+    color,
+    tempVals,
+    requestPermissions,
+    toggleLED,
+  } = useBLE();
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
+  const item = ({ item }) => (
+    <ThemedView style={{ flexDirection: "row" }}>
+      <ThemedView style={{ width: 200, backgroundColor: "lightyellow" }}>
+        <ThemedText style={{ fontSize: 16, fontWeight: "bold", textAlign: "center" }}>
+          {JSON.stringify(item.timestamp)}
+          {/* item ? {item.timestamp.getFullYear()}-{item.timestamp.getMonth()}-{item.timestamp.getDate()} {item.timestamp.getHours()}:{item.timestamp.getMinutes()} : "" */}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={{ width: 100, backgroundColor: "lightpink" }}>
+        <ThemedText style={{ fontSize: 16, fontWeight: "bold", textAlign: "center" }}>
+          {JSON.stringify(item.value)}
+        </ThemedText>
+      </ThemedView>
+    </ThemedView>
+  );
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
@@ -23,101 +71,52 @@ export default function CollectScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Collect</ThemedText>
       </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/collect.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ alignSelf: "center" }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-          to see how to load{" "}
-          <ThemedText style={{ fontFamily: "SpaceMono" }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
+
+      {connectedDevice ? (
+          <>
+            <ThemedText
+              style={{ backgroundColor: color }}
+            >
+              Connected
             </ThemedText>
-          ),
-        })}
-      </Collapsible>
+            <ThemedView
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <FlatList
+                data={tempVals}
+                renderItem={item}
+                scrollEnabled={false}
+              />
+            </ThemedView>
+            {`\n${JSON.stringify(tempVals)}`}
+          </>
+        ) : (
+          <ThemedText>
+            Please connect the ESP32-WIOT2
+          </ThemedText>
+        )}
+
+      <TouchableOpacity onPress={openModal} style={styles.ctaButton}>
+        <ThemedText style={styles.ctaButtonText}>Connect</ThemedText>
+      </TouchableOpacity>
+
+      <DeviceModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDevice}
+        devices={allDevices}
+      />
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: "#118BAB",
+    color: "#808080",
     bottom: -90,
     left: -35,
     position: "absolute",
@@ -125,5 +124,19 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: "row",
     gap: 8,
+  },
+  ctaButton: {
+    backgroundColor: "#FF6060",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
   },
 });
---
+++ app/useBLE.ts
@@ -0,0 +1,259 @@
+/* eslint-disable no-bitwise */
+import { useEffect, useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+
+import * as ExpoDevice from "expo-device";
+
+import base64 from "react-native-base64";
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+} from "react-native-ble-plx";
+
+const DATA_SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const COLOR_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1217";
+const LED_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1220";
+const TEMP_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1218";
+
+const bleManager = new BleManager();
+
+function useBLE() {
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [color, setColor] = useState("white");
+  const [tempVals, setTempVals] = useState<Object[]>([]);
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth Low Energy requires Location",
+            buttonPositive: "OK",
+          }
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () => {
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+
+      if (
+        device &&
+        (device.localName === "ESP32-WIOT2" || device.name === "ESP32-WIOT2")
+      ) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicateDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+  };
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(deviceConnection);
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      bleManager.stopDeviceScan();
+
+      // startStreamingData(deviceConnection);
+      startReadingTemp(deviceConnection);
+    } catch (e) {
+      console.log("FAILED TO CONNECT", e);
+    }
+  };
+
+  const onDataUpdate = (
+    error: BleError | null,
+    characteristic: Characteristic | null
+  ) => {
+    if (error) {
+      console.log(error);
+      return;
+    } else if (!characteristic?.value) {
+      console.log("No Data was received");
+      return;
+    }
+
+    const colorCode = base64.decode(characteristic.value);
+
+    let color = "white";
+    if (colorCode === "B") {
+      color = "blue";
+    } else if (colorCode === "R") {
+      color = "red";
+    } else if (colorCode === "G") {
+      color = "green";
+    }
+
+    setColor(color);
+  };
+
+  const startStreamingData = async (device: Device) => {
+    if (device) {
+      device.monitorCharacteristicForService(
+        DATA_SERVICE_UUID,
+        COLOR_CHARACTERISTIC_UUID,
+        onDataUpdate
+      );
+    } else {
+      console.log("No Device Connected");
+    }
+  };
+
+  const toggleLED = async (device: Device) => {
+    if (device) {
+      const characteristic = await device.readCharacteristicForService(
+        DATA_SERVICE_UUID,
+        LED_CHARACTERISTIC_UUID
+      );
+
+      if (!characteristic?.value) {
+        console.log("No Data was received");
+        return;
+      }
+
+      const read_val = base64.decode(characteristic.value);
+      console.log(read_val);
+      device.writeCharacteristicWithResponseForService(
+        DATA_SERVICE_UUID,
+        LED_CHARACTERISTIC_UUID,
+        base64.encode(read_val === "OFF" ? "ON" : "OFF")
+      );
+    } else {
+      console.log("No Device Connected");
+    }
+  };
+
+  const startReadingTemp = async (device: Device | null) => {
+    const interval = setInterval(() => {
+      readTemp(device);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  };
+
+  const readTemp = async (device: Device | null) => {
+    if (device) {
+      const characteristic = await device.readCharacteristicForService(
+        DATA_SERVICE_UUID,
+        TEMP_CHARACTERISTIC_UUID
+      );
+
+      if (!characteristic?.value) {
+        console.log("No Data was received");
+        return;
+      }
+
+      // const readVal = base64.decode(characteristic.value);
+      const TZ_OFFSET = 8;
+      const readVal =
+        "2024112311440001.69,11450020.10,11460100.09,11472000.60|11463000.07,11470000.00,";
+
+      let tempValsBuffer = [];
+
+      let start = 8;
+      let end = 19;
+
+      let currDate = new Date(
+        `${readVal.substring(0, 4)}-${readVal.substring(
+          4,
+          6
+        )}-${readVal.substring(6, 8)}`
+      );
+      while (end < readVal.length) {
+        if (readVal.charAt(start - 1) === "|") {
+          currDate = new Date(currDate.getTime() + 86400000);
+        }
+
+        tempValsBuffer.push({
+          timestamp: new Date(
+            currDate.getTime() +
+              parseInt(readVal.substring(start, start + 2)) * 3600000 +
+              parseInt(readVal.substring(start + 2, start + 4)) * 60000 -
+              TZ_OFFSET * 3600000
+          ).toJSON(),
+          value: parseFloat(readVal.substring(start + 4, end)),
+        });
+        console.log(tempValsBuffer);
+        start += 12;
+        end += 12;
+      }
+
+      setTempVals(tempValsBuffer);
+      console.log(readVal);
+    } else {
+      console.log("No Device Connected");
+    }
+  };
+
+  return {
+    connectToDevice,
+    allDevices,
+    scanForPeripherals,
+    connectedDevice,
+    color,
+    tempVals,
+    requestPermissions,
+    toggleLED,
+  };
+}
+
+export default useBLE;
